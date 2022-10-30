94b5d0414ff48ec39caeaaada7d46b9bc01a02fb
Eric Askew <Easkew81@yahoo.com>
2022-10-30 13:27:49 -0400

    ExperinceSection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro/Intro';
 import Services from './components/Services/Services';
+import Experience from './components/Experience/Experience';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
        <Navbar/>
        <Intro/>
        <Services/>
+       <Experience/>
     </div>
   );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,25 @@
+import "./Experience.css"
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="achievement">
+        <div className="circle">3+</div>
+        <span>Years</span>
+        <span>Experience</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">10+</div>
+        <span>Projects</span>
+        <span>Completed</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">5+</div>
+        <span>Open-Source</span>
+        <span>Companies</span>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
